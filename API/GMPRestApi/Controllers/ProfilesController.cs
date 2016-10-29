5da87bd7e29525e3739959891fbef119b3071ed4
CORP\Rachel.Breeze <rachel.breeze@sigma.se>
2016-10-29 19:20:14 +0100

    First pass at profile data

---
+++ API/GMPRestApi/Controllers/ProfilesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using GMPRestApi.Models;
+using GMPRestApi.Models.Data;
+
+namespace GMPRestApi.Controllers
+{
+    [System.Web.Mvc.RoutePrefix("api/Profiles")]
+    public class ProfilesController : ApiController
+    {
+        // GET: Profiles
+        // GET api/<controller>
+        public List<Profile> Get()
+        {
+            List<Profile> retProfiles = new List<Profile>();
+
+            GMPRestApi.Models.Data.GMPMissingPersonEntities entities = new GMPRestApi.Models.Data.GMPMissingPersonEntities();
+            foreach (var person in entities.misper_.Where(x => x.Misper_Extended.Count > 0))
+            {
+                Profile profile = new Profile();
+                Misper_Extended extended = person.Misper_Extended.FirstOrDefault();
+                profile.LastSeen = person.Date_Went_Missing.Value;
+                profile.Latitude = extended.Latitude.Value;
+                profile.Longtitude = extended.Longitude.Value;
+                profile.ProfileURL = "tbd";
+                profile.UserID = person.Unique_ID;
+                if (!string.IsNullOrEmpty(person.Birth_Year))
+                {
+                    profile.Age = DateTime.Now.Year - int.Parse(person.Birth_Year);
+                }
+                profile.Forename = person.Forenames;
+                profile.Surname = person.Surname;
+                profile.IsDangerous = extended.Is_Dangerous == "Y";
+                retProfiles.Add(profile);
+            }
+
+            return retProfiles;
+        }
+
+
+   }
+}
---
+++ API/GMPRestApi/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace GMPRestApi.Models
+{
+    public class Profile
+    {
+        public string UserID { get; set; }
+        public double Latitude { get; set; }
+        public double Longtitude { get; set; }
+        public DateTime LastSeen { get; set; }
+        public string ProfileURL { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public bool IsDangerous { get; set; }
+    }
+}

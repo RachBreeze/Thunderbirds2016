8ccf157b9e35638e4069e70b3fdca1fe6483e194
CORP\Rachel.Breeze <rachel.breeze@sigma.se>
2016-10-30 13:50:52 +0000

    Added instagram controller and tweaked tweet contoller.

---
+++ API/GMPRestApi/Models/Instagram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tweetinvi.Logic.DTO;
+
+namespace GMPRestApi.Models
+{
+    public class Instagram
+    {
+        public string UserID { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime DateTime { get; set; }
+        public string InstagramName { get; set; }
+        public string PostUrl { get; set; }
+    }
+}

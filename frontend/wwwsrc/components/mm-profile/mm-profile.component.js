a3fb415f98c0d1fccce12b953878a44a5b35e8ab
Ash Prosser <ashprosser@live.co.uk>
2016-10-31 13:04:37 +0000

    Final commit.

--- frontend/wwwsrc/components/mm-profile/mm-profile.component.js
+++ frontend/wwwsrc/components/mm-profile/mm-profile.component.js
@@ -1,3 +1,5 @@
+let mock = [{"IsVerified":false,"DateTime":"2016-10-30T00:00:00","LocationType":"App Sighting","UserID":"383A353A3936393A3139","Latitude":53.53257,"Longitude":-2.406005,"ContactNumber":null,"DeviceID":null},{"IsVerified":false,"DateTime":"2016-10-30T00:00:00","LocationType":"App Sighting","UserID":"383A353A3936393A3139","Latitude":53.5938,"Longitude":-2.386993,"ContactNumber":null,"DeviceID":null},{"IsVerified":false,"DateTime":"2016-10-30T00:00:00","LocationType":"App Sighting","UserID":"383A353A3936393A3139","Latitude":53.54896,"Longitude":-2.13031,"ContactNumber":null,"DeviceID":null},{"IsVerified":false,"DateTime":"2016-10-30T00:00:00","LocationType":"App Sighting","UserID":"383A353A3936393A3139","Latitude":53.49519,"Longitude":-2.273497,"ContactNumber":null,"DeviceID":null},{"IsVerified":false,"DateTime":"2016-10-30T00:00:00","LocationType":"App Sighting","UserID":"383A353A3936393A3139","Latitude":53.44915,"Longitude":-2.114283,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Haunt","UserID":"383A353A3936393A3139","Latitude":53.54351,"Longitude":-2.62281,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Haunt","UserID":"383A353A3936393A3139","Latitude":53.4792,"Longitude":-2.237046,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Haunt","UserID":"383A353A3936393A3139","Latitude":53.53413,"Longitude":-2.143422,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.41793,"Longitude":-2.432691,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.49283,"Longitude":-2.070066,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.50923,"Longitude":-2.525777,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.46584,"Longitude":-2.228761,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.58996,"Longitude":-2.356951,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.54483,"Longitude":-2.197899,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.59935,"Longitude":-2.28168,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.46584,"Longitude":-2.228761,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.39466,"Longitude":-2.266324,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.39184,"Longitude":-2.214378,"ContactNumber":null,"DeviceID":null},{"IsVerified":true,"DateTime":"2016-10-30T00:00:00","LocationType":"Searched","UserID":"383A353A3936393A3139","Latitude":53.51766,"Longitude":-2.228723,"ContactNumber":null,"DeviceID":null}];
+
 import template from './mm-profile.html';
 
 export default {
@@ -5,7 +7,7 @@ export default {
     bindings: {
         profile: '<'
     },
-    controller: function (AlzimersPattern,ToddlerPattern, mockSupermarkets, Sighting) {
+    controller: function (AlzimersPattern,ToddlerPattern, $q, API_DOWN, mockSupermarkets, Sighting) {
         "ngInject";
 
         let styles = [{"featureType":"administrative.province","elementType":"geometry.fill","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffe4e4"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffd5db"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#bddcff"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#eef5fd"}]}];
@@ -56,7 +58,15 @@ export default {
         this.syncSightings = () => {
             this.loading = true;
 
-            return Sighting.query().$promise.then((sightings) => {
+            let $p;
+
+            if (API_DOWN) {
+                $p = $q.resolve(mock);
+            } else {
+                $p = Sighting.query().$promise;
+            }
+
+            return $p.then((sightings) => {
                 this.sightings = sightings;
             }).finally(() => {
                 this.loading = false;
